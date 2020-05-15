196ce64f2fd0b21561a8729fe0870161c25afd33
weifucheng <39686870+weifucheng1102@users.noreply.github.com>
2020-05-15 14:43:55 +0800

    判断player是否存在

--- android/src/main/java/com/ryanheise/just_audio/AudioPlayer.java
+++ android/src/main/java/com/ryanheise/just_audio/AudioPlayer.java
@@ -82,7 +82,7 @@ public class AudioPlayer implements MethodCallHandler, Player.EventListener {
 		});
 		state = PlaybackState.none;
 
-		player = new SimpleExoPlayer.Builder(context).build();
+		player = player?player:new SimpleExoPlayer.Builder(context).build();
 		player.addListener(this);
 		registLisener(registrar.context().getApplicationContext());
 	}
